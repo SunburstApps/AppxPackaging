c287975e004a4a70f5ca909da9e0a81760bb6b68
William Kent <wjk011@gmail.com>
2017-04-09 15:38:28 -0400

    Add Sunburst.AppxPackaging.PriAssemblyResolver project
    This project contains code adapte from one of Microsoft's
    Desktop Bridge samples. It can be used to load localized
    satellite assemblies from an AppX package in a way that
    allows unused languages to not be installed, and have the
    app still function.
    
    Note that this project doesn't yet compile, because it is
    missing references. Those will be added over the next few commits.

---
+++ Sunburst.AppxPackaging.PriAssemblyResolver/PriAssemblyResolver.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Resources.Core;
+
+namespace Sunburst.AppxPackaging
+{
+    public static class PriAssemblyResolver
+    {
+        static class KnownMrtFailureCodes
+        {
+            // The HRESULT thrown when MRT is used in a normal desktop app
+            internal const int MrtNeedsAppXPackagedApp = -2147009760;
+
+            // The HRESULT thrown when a file is missing from the package
+            internal const int MissingDefaultOrNeutralResource = -2147009780;
+        }
+
+        public static bool IsSupported
+        {
+            get
+            {
+                try
+                {
+                    // If this property call succeeds, we are running in a package.
+                    // (If we are not, it will throw an exception.)
+                    var unused = Package.Current.Id;
+                    return true;
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+        }
+
+        public static void Enable()
+        {
+            AppDomain.CurrentDomain.AssemblyResolve += AssemblyResolveHandler;
+        }
+
+        private static Assembly AssemblyResolveHandler(object sender, ResolveEventArgs args)
+        {
+            AssemblyName fullName = new AssemblyName(args.Name);
+            string desiredFileName = fullName.Name;
+            string desiredCulture = fullName.CultureName;
+
+            try
+            {
+                ResourceContext context = ResourceContext.GetForViewIndependentUse();
+                context.Languages = new[] { desiredCulture };
+
+                ResourceManager manager = ResourceManager.Current;
+                ResourceMap fileResources = manager.MainResourceMap.GetSubtree("Files");
+
+                // This is the filename as it appeared on-disk when the package was created. At a minimum, you
+                // need to add ".dll" on the end, but you might also need to prepend a folder name or do other
+                // modifications to match your configuration.
+                string targetFileName = $"{desiredFileName}.dll";
+
+                NamedResource desiredResource = fileResources[targetFileName];
+                ResourceCandidate bestCandidate = desiredResource.Resolve(context);
+                string absoluteFileName = bestCandidate.ValueAsString;
+                return LoadAssemblyFromPackageGraph(Package.Current, absoluteFileName);
+            }
+            catch (Exception ex) when (ex.HResult == KnownMrtFailureCodes.MrtNeedsAppXPackagedApp)
+            {
+                throw new InvalidOperationException("PRI-based assembly resolving only works in an AppX-packaged (Centennial) app.");
+            }
+            catch (Exception ex) when (ex.HResult == KnownMrtFailureCodes.MissingDefaultOrNeutralResource)
+            {
+                throw new FileNotFoundException("Missing AppX resource", desiredFileName, ex);
+            }
+        }
+
+        private static Assembly LoadAssemblyFromPackageGraph(Package package, string absolutePath)
+        {
+            if (IsFilePathInDirectory(absolutePath, package.InstalledLocation.Path))
+                return Assembly.UnsafeLoadFrom(absolutePath);
+
+            foreach (var dep in package.Dependencies)
+                if (IsFilePathInDirectory(absolutePath, dep.InstalledLocation.Path))
+                    return Assembly.UnsafeLoadFrom(absolutePath);
+
+            throw new UnauthorizedAccessException("Attempted to load assembly from outside package graph");
+        }
+
+        private static bool IsFilePathInDirectory(string absoluteFile, string absoluteDirectory)
+        {
+            var fileName = new FileInfo(absoluteFile).FullName;
+            var directoryName = new DirectoryInfo(absoluteDirectory).FullName;
+
+            if (!directoryName.EndsWith(Path.DirectorySeparatorChar.ToString()))
+                directoryName += Path.DirectorySeparatorChar;
+
+            int index = string.Compare(fileName, 0, directoryName, 0, directoryName.Length, StringComparison.OrdinalIgnoreCase);
+            return index == 0;
+        }
+    }
+}
---
+++ Sunburst.AppxPackaging.PriAssemblyResolver/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sunburst.AppxPackaging.PriResourceLoader")]
+[assembly: AssemblyCompany("William Kent")]
+[assembly: AssemblyProduct("Sunburst.AppxPackaging.PriResourceLoader")]
+[assembly: AssemblyCopyright("Copyright © William Kent 2017. All rights reserved.")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: ComVisible(false)]
