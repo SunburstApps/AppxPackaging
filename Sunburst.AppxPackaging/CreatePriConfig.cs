42ca3238865c2fe102879ea403de77e5b9d6bda0
William Kent <wjk011@gmail.com>
2017-04-09 10:57:46 -0400

    Add CreatePriConfig task

---
+++ Sunburst.AppxPackaging/CreatePriConfig.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace Sunburst.AppxPackaging
+{
+    public sealed class CreatePriConfig : ToolTask
+    {
+        [Required]
+        public string[] LanguageQualifiers { get; set; }
+        public string TargetVersion { get; set; } = null;
+        [Required]
+        public string ConfigFilePath { get; set; }
+
+        protected override string ToolName => "makepri.exe";
+
+        protected override string GenerateFullPathToTool()
+        {
+            return @"C:\Program Files (x86)\Windows Kits\10\bin\10.0.15063.0\x86\makepri.exe";
+        }
+
+        protected override string GenerateCommandLineCommands()
+        {
+            StringBuilder qualifierString = new StringBuilder();
+            qualifierString.Append(string.Join("_", LanguageQualifiers));
+
+            string runtimeVersion = TargetVersion ?? "10.0.0.0";
+
+            List<string> argv = new List<string>();
+            argv.Add("createconfig");
+            argv.Add("/cf");
+            argv.Add(ConfigFilePath);
+            argv.Add("/dq");
+            argv.Add(qualifierString.ToString());
+            argv.Add("/pv");
+            argv.Add(runtimeVersion);
+            argv.Add("/o");
+
+            return string.Join(" ", argv.Select(arg => $"\"{arg}\""));
+        }
+    }
+}
