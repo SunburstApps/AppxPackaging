985ecec1b32afab4bddda3acc2fd8b70a5ed474e
William Kent <wjk011@gmail.com>
2017-04-09 15:50:42 -0400

    Add Sunburst.UwpApiAnalyzer project
    This project (derived from the UWPDesktop nuget package) contains
    a Roslyn analyzer that will issue warnings when UWP APIs not available
    in Centennial applications are used.
    
    A targets file to reference this assmbly, as well as the WinMD metadata
    files themselves, will be added next commit.

---
+++ Sunburst.AppxPackaging.UwpApiAnalyzer/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sunburst.AppxPackaging.UwpApiAnalyzer")]
+[assembly: AssemblyCompany("William Kent")]
+[assembly: AssemblyProduct("Sunburst.AppxPackaging.UwpApiAnalyzer")]
+[assembly: AssemblyCopyright("Copyright © 2017 William Kent. All rights reserved.")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: ComVisible(false)]
