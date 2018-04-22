47778fa989952dff95de4d22065f3fefdc4b80b5
William Kent <wjk@users.noreply.github.com>
2018-04-22 19:17:45 -0400

    Fix handling of @(Content) items

---
+++ src/Sunburst.AppxPackaging.BuildTasks/AppxConvertContentItems.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace Sunburst.AppxPackaging.BuildTasks
+{
+    public sealed class AppxConvertContentItems : Task
+    {
+        [Required]
+        public string RootDestination { get; set; }
+
+        [Required]
+        public ITaskItem[] InputItems { get; set; }
+
+        [Output]
+        public ITaskItem[] OutputItems { get; set; }
+
+        public override bool Execute()
+        {
+            List<ITaskItem> outputs = new List<ITaskItem>();
+
+            foreach (ITaskItem item in InputItems)
+            {
+                Log.LogMessage(MessageImportance.Normal, "Processing {0}", item.ItemSpec);
+                string copyPolicy = item.GetMetadata("CopyToOutputDirectory") ?? "";
+                if (!(copyPolicy == "Always" || copyPolicy == "PreserveNewest")) continue;
+
+                string destination;
+                string link = item.GetMetadata("Link");
+                if (!string.IsNullOrEmpty(link))
+                {
+                    destination = Path.Combine(RootDestination, Path.GetDirectoryName(link));
+                }
+                else
+                {
+                    destination = RootDestination;
+                }
+
+                Log.LogMessage(MessageImportance.Normal, "-> {0}", destination);
+                ITaskItem newItem = new TaskItem(item);
+                newItem.SetMetadata("Destination", destination);
+                outputs.Add(newItem);
+            }
+
+            OutputItems = outputs.ToArray();
+            return true;
+        }
+    }
+}
